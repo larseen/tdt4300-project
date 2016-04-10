e634affb0f4c87f9e46370468d08f6c74e94e479
Kristoffer Larsen <kristoffer@larsen.so>
2016-04-10 19:31:35 +0200

    edited README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tdt4300-project
+
+The code in this repository assumes that you have the following files in this directory:
+
+- dataset_TIST2015.tsv
+- dataset_TIST2015_Citues.txt
+- geotweets.tsv
+- negative-words.txt
+- positive-words.txt
--- test.py
+++
@@ -1,9 +0,0 @@
-lines = []
-astring = " Lorem    upgradable ipsum   do lor sit amet, consectetur  ad  ipisicing elit, sed do upliftingly eiusmod tempor  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-with open('positive-words.txt', 'r') as text_file:
-    lines = text_file.read().split('\n')
-
-
-alist = astring.split(' ')
-
-print(alist)
