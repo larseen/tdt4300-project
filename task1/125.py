8a7c23a078c705f55a15a8cfcafcdec116847794
Olav Markussen <olavmarkussen@hotmail.no>
2016-04-24 13:38:14 +0200

    Move 125.py to correct folder

--- task1/125.py
+++ task1/125.py
@@ -4,8 +4,9 @@ from operator import add
 from operator import concat
 from collections import Counter
 
-sc = SparkContext("local", "tdt4300")
-rawData = sc.textFile("../dataset_TIST2015.tsv", use_unicode=False)
+sc = SparkContext("local[*]", "tdt4300")
+rawData = sc.textFile("../dataset_TIST2015.tsv",
+        use_unicode=False).cache()
 
 header = rawData.first()
 data = rawData.filter(lambda x: x != header)
@@ -14,8 +15,9 @@ dataWithoutWhite = data.map(lambda x: x.split('\n')[0].split('\t'))
 
 dataTouples = dataWithoutWhite\
     .map(lambda x: (x[2], x[0]))\
-    .groupByKey().map(lambda x : (x[0], len(x[1])))\
-    .countByValue().map(lambda x : x[1]).items()\
-    .take(10)\
+    .groupByKey()\
+    .map(lambda x : (len(x[1]), x[0]))\
+    .groupByKey()\
+    .map(lambda x: (x[0], len(x[1])))
 
-print(dataTouples)
+print(dataTouples.take(10))
