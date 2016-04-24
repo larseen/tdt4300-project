a7cfed3945b1c79b64008db72e7cae759d7af619
Olav Markussen <olavmarkussen@hotmail.no>
2016-04-24 13:35:01 +0200

    Fix calculation of length of sessions
    Might not be the most optimal solution.

---
+++ 125.py
@@ -0,0 +1,23 @@
+from pyspark import SparkContext
+from datetime import datetime, timedelta
+from operator import add
+from operator import concat
+from collections import Counter
+
+sc = SparkContext("local[*]", "tdt4300")
+rawData = sc.textFile("../dataset_TIST2015.tsv",
+        use_unicode=False).cache()
+
+header = rawData.first()
+data = rawData.filter(lambda x: x != header)
+
+dataWithoutWhite = data.map(lambda x: x.split('\n')[0].split('\t'))
+
+dataTouples = dataWithoutWhite\
+    .map(lambda x: (x[2], x[0]))\
+    .groupByKey()\
+    .map(lambda x : (len(x[1]), x[0]))\
+    .groupByKey()\
+    .map(lambda x: (x[0], len(x[1])))
+
+print(dataTouples.take(10))
